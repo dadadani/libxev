2e02b73c172e5aee0f2db253d3f560a16bce9e33
Sythivo <69376875+Sythivo@users.noreply.github.com>
2025-08-22 14:17:51 -0400

    fix IOCP counting

--- src/backend/iocp.zig
+++ src/backend/iocp.zig
@@ -439,7 +439,8 @@ pub const Loop = struct {
     }
 
     pub fn countPending(self: *Loop) usize {
-        return self.active + @as(u1, if (self.completions.empty()) 0 else 1);
+        return self.active + @as(u1, if (self.submissions.empty()) 0 else 1) +
+            @as(u1, if (self.completions.empty()) 0 else 1);
     }
 
     /// Add a timer to the loop. The timer will execute in "next_ms". This is oneshot: the timer
